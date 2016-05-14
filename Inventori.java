c631df2f81dc80c86394fad62e33bccdd75c2a3d
kambingtampan <feisalramadhanm@gmail.com>
2016-05-14 09:10:12 +0700

    Update Inventori.java

--- Inventori.java
+++ Inventori.java
@@ -1,8 +1,14 @@
 //Masih belum, dibuat cuma untuk membantu instansiasi inventori pemain
-abstract class Inventori {
-   private boolean status;
-    
-   abstract boolean setStatus(boolean status);
+public class Inventori<T> {
+	private boolean status;
+	private T bag[];
    
+	public Inventori (T bag[], T isi){
+	   int i;
+	   
+	   T bag[i]=isi;
+	   i++;
+	}  
+	
    abstract void aksi();
 }
